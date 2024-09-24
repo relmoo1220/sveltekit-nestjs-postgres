d4f7c07a29ec1005f081e41d59416d2d6dec04ff
relmoo1220 <gamingwithmura@gmail.com>
2024-09-24 22:37:20 +0800

    Frontend done

---
+++ frontend/src/lib/components/SectionWrapper.svelte
@@ -0,0 +1,10 @@
+<script>
+
+</script>
+
+<section class={"flex flex-col px-4 "}>
+    <div class="flex flex-col flex-1 max-w-[1400px] mx-auto w-full items-center">
+        <!-- Insert any content that is placed between the opening and closing tags of the component -->
+        <slot />
+    </div>
+</section>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import SectionWrapper from "$lib/components/SectionWrapper.svelte";
+  import Button from "$lib/components/ui/button/button.svelte";
+  import Input from "$lib/components/ui/input/input.svelte";
+  import Label from "$lib/components/ui/label/label.svelte";
+  import Pagination from "$lib/components/ui/pagination/pagination.svelte";
+  import * as Table from "$lib/components/ui/table/index.js";
+</script>
+
+<div class="h-screen w-full bg-slate-950">
+  <SectionWrapper>
+    <h1 class="font-bold font-mono text-white text-2xl">
+      📋 CSV Uploader Web Application 📋
+    </h1>
+    <p class="font-mono text-gray-400">
+      This project is to showcase a full stack development
+    </p>
+  </SectionWrapper>
+  <SectionWrapper>
+    <div class="mt-5">
+      <Label class="font-bold text-gray-400 mt-10 mb-2" for="csvFile"
+        >CSV File</Label
+      >
+      <Input type="file" id="csvFile" class="w-[250px]" />
+    </div>
+    <Button class="mt-2">Upload</Button>
+  </SectionWrapper>
+  <SectionWrapper>
+    <Input type="text" placeholder="Search" class="max-w-xs m-10" />
+  </SectionWrapper>
+  <SectionWrapper>
+    <Table.Root>
+      <Table.Caption>A list of posts.</Table.Caption>
+      <Table.Header>
+        <Table.Row>
+          <Table.Head class="w-[100px]">id</Table.Head>
+          <Table.Head>postId</Table.Head>
+          <Table.Head>name</Table.Head>
+          <Table.Head>email</Table.Head>
+          <Table.Head class="text-right">body</Table.Head>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body></Table.Body>
+    </Table.Root>
+  </SectionWrapper>
+  <SectionWrapper></SectionWrapper>
+</div>
